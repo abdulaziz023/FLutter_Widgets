5e9f4717d35b769abbba1a278a76f307b2f475e8
abdulaziz <thugstools966@gmail.com>
2024-09-16 15:28:31 +0500

    Image and Network Image

--- lib/src/feature/home_page.dart
+++ lib/src/feature/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/image/car.jpg"),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fauto.goodfon.com%2Fmitsubishi%2Fwallpaper-mitsubishi-lancer-evolution-x-3892.html&psig=AOvVaw2e5b4eZtjZ3_Zpsap3zdZS&ust=1726565736167000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjG6uuUx4gDFQAAAAAdAAAAABAd"),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          Image.asset(
+            "assets/image/car.jpg",
+          ),
+          Image.network("URL")
+        ],
+      ),
+    );
   }
 }
