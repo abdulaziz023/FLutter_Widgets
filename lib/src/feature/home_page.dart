3341e1233065d44042f01447d3527f711b8673df
abdulaziz <thugstools966@gmail.com>
2024-09-28 15:19:14 +0500

    TextFormField prefix suffix hintText prefixText suffixText

--- lib/src/feature/home_page.dart
+++ lib/src/feature/home_page.dart
@@ -38,24 +38,28 @@ class _HomePageState extends State<HomePage> {
               // minLines: 5,
               // maxLines: 10,
               decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(color: Colors.blue),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(color: Colors.yellow),
-                ),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(color: Colors.red),
-                ),
-              ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.yellow),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  prefix: Icon(Icons.person),
+                  suffix: Icon(Icons.email),
+                  hintText: "Pochta kiriting",
+                  labelText: "Label text",
+                  counterText: "Counter"),
               cursorColor: Colors.red,
               // onChanged: (value) {
               //   print("On changed $value");
