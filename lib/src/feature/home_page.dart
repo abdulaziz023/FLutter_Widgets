6810e5ae790e672d75c1431526634fc49994a782
abdulaziz <thugstools966@gmail.com>
2024-11-12 12:11:35 +0500

    GridView.Builder

--- lib/src/feature/home_page.dart
+++ lib/src/feature/home_page.dart
@@ -12,7 +12,16 @@ class _HomePageState extends State<HomePage> {
   final PageController pageController = PageController();
   int index = 0;
   List<String> names = ["Abdulloh", "Akmal", "Anvar", "Qodir"];
-  List<Color> colors = [Colors.red, Colors.yellow, Colors.green, Colors.black];
+  List<Color> colors = [
+    Colors.red,
+    Colors.yellow,
+    Colors.green,
+    Colors.black,
+    Colors.red,
+    Colors.yellow,
+    Colors.green,
+    Colors.black,
+  ];
   ScrollController controller = ScrollController();
 
   @override
@@ -25,42 +34,20 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: GridView.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                childAspectRatio: 1,
-                controller: controller,
-                padding: const EdgeInsets.all(10),
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                children: const [
-                  ColoredBox(color: Colors.red),
-                  ColoredBox(color: Colors.yellow),
-                  ColoredBox(color: Colors.green),
-                  ColoredBox(color: Colors.blue),
-                  ColoredBox(color: Colors.red),
-                  ColoredBox(color: Colors.yellow),
-                  ColoredBox(color: Colors.green),
-                  ColoredBox(color: Colors.blue),
-                  ColoredBox(color: Colors.red),
-                  ColoredBox(color: Colors.yellow),
-                  ColoredBox(color: Colors.green),
-                  ColoredBox(color: Colors.blue),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Text("SALOMM"),
-            )
-          ],
-        ),
-      ),
+          body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            crossAxisSpacing: 10,
+            mainAxisExtent: 100,
+            mainAxisSpacing: 10,
+            childAspectRatio: 10),
+        itemCount: 8,
+        itemBuilder: (BuildContext context, int index) {
+          return ColoredBox(
+            color: colors[index],
+          );
+        },
+      )),
     );
   }
 }
