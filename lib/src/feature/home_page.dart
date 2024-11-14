c2f103f740d45350affd96c87ae604b76fda7821
abdulaziz <thugstools966@gmail.com>
2024-11-14 14:11:08 +0500

    PageView and customTabPage

--- lib/src/feature/home_page.dart
+++ lib/src/feature/home_page.dart
@@ -34,40 +34,92 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: RefreshIndicator(
-          backgroundColor: Colors.yellow,
-          color: Colors.red,
-          // displacement: 50,
-          // edgeOffset: 200,
-          semanticsLabel: 'salom',
-          strokeWidth: 5,
-
-          onRefresh: () async {
-            return Future.delayed(
-              const Duration(seconds: 1),
-              () {
-                print("Refresh qildiz");
-              },
-            );
-          },
-          child: GridView(
-            padding: EdgeInsets.all(20),
-            //ALign and Alignment
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                crossAxisSpacing: 30,
-                mainAxisSpacing: 10),
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(18),
-                child: ColoredBox(color: Colors.red),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                onPageChanged: (value) {
+                  setState(() {
+                    index = value;
+                  });
+                },
+                controller: pageController,
+                children: const [
+                  Center(
+                    child: SizedBox(
+                      height: 400,
+                      width: 400,
+                      child: ColoredBox(color: Colors.black),
+                    ),
+                  ),
+                  Center(
+                    child: SizedBox(
+                      height: 400,
+                      width: 400,
+                      child: ColoredBox(color: Colors.pink),
+                    ),
+                  ),
+                  Center(
+                    child: SizedBox(
+                      height: 400,
+                      width: 400,
+                      child: ColoredBox(color: Colors.yellow),
+                    ),
+                  ),
+                  Center(
+                    child: SizedBox(
+                      height: 400,
+                      width: 400,
+                      child: ColoredBox(color: Colors.green),
+                    ),
+                  ),
+                ],
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(18),
-                child: ColoredBox(color: Colors.yellow),
-              ),
-            ],
-          ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      pageController.animateToPage(index - 1,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut);
+                    });
+                  },
+                  icon: const Icon(Icons.arrow_left),
+                ),
+                Icon(
+                  Icons.circle_rounded,
+                  color: index == 0 ? Colors.red : Colors.black,
+                ),
+                const SizedBox(width: 5),
+                Icon(
+                  Icons.circle_rounded,
+                  color: index == 1 ? Colors.red : Colors.black,
+                ),
+                const SizedBox(width: 5),
+                Icon(
+                  Icons.circle_rounded,
+                  color: index == 2 ? Colors.red : Colors.black,
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      pageController.animateToPage(index + 1,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut);
+                    });
+                  },
+                  icon: const Icon(Icons.arrow_right),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            )
+          ],
         ),
       ),
     );
